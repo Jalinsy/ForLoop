6c8d5f605a23e7f0f908d21e0005406fded6e3d8
jiaji004 <jiaji200324@gmail.com>
2023-05-08 16:26:33 +0800

    test

---
+++ TestForLoop.java
@@ -0,0 +1,8 @@
+public class TestForLoop {
+	public static void main(String[] args) {
+		for(int x = 10;x < 34;x = x+2) {
+			System.out.println("x的值是" + x);
+			System.out.println("\n");
+		}
+	}
+}
